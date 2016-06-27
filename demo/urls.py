61ebf2529957e377b8b31da4d964f68f931aea1e
Jonathan <jonathan.pastor@me.com>
2016-06-27 11:45:31 -0500

    towards a first online documentation for the API;

--- demo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from demo import views
-
-
-urlpatterns = [
-    url(r'^processdef_form/',  views.create_processdef, name='processdef_form'),
-    url(r'^processes/',  views.processes, name='processes'),
-]
--- prapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import ProcessDefinition
-
-admin.site.register(ProcessDefinition)
