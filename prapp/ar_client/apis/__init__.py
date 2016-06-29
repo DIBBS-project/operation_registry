2292be436c40030d97c0a2453cc465b9a255f6a4
Maverick Chardet <chardetm@gmail.com>
2016-06-29 17:03:10 -0500

    Updated the swagger client for appliance registry

--- prapp/ar_client/apis/__init__.py
+++ prapp/ar_client/apis/__init__.py
@@ -4,4 +4,5 @@ from __future__ import absolute_import
 from .actions_api import ActionsApi
 from .appliances_api import AppliancesApi
 from .scripts_api import ScriptsApi
+from .sites_api import SitesApi
 from .users_api import UsersApi
