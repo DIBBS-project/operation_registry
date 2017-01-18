c1949724b3123395b5983e62e2e0fc84d4339206
Nick Timkovich <prometheus235@gmail.com>
2017-01-18 14:38:27 -0600

    Remove old auth endpoints

--- orapp/models.py
+++ orapp/models.py
@@ -1,32 +1,23 @@
-from django.db import models
-from django.contrib.auth.models import User
-from jsonfield import JSONField
-
+# coding: utf-8
+from __future__ import absolute_import, print_function, unicode_literals
 
 from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
-
-# Create your models here.
+from django.db import models
+from jsonfield import JSONField
 
 
 class Operation(models.Model):
     name = models.CharField(max_length=256)
     logo_url = models.CharField(max_length=512, blank=True, default='')
-    author = models.ForeignKey('auth.User', related_name='operation_definitions')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='operation_definitions')
     description = models.TextField(blank=True, default='')
     string_parameters = JSONField(blank=True, default='[]')
     file_parameters = JSONField(blank=True, default='[]')
 
-    # @property
-    # def __str__(self):
-    #     return self.name
-
 
 class OperationVersion(models.Model):
     name = models.CharField(max_length=256)
-    author = models.ForeignKey('auth.User', related_name='process_implementations')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='process_implementations')
     operation = models.ForeignKey(Operation, related_name='implementations')
     appliance = models.CharField(max_length=256)
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -34,10 +25,3 @@ class OperationVersion(models.Model):
     script = models.TextField()
     output_type = models.CharField(max_length=256)
     output_parameters = JSONField(blank=True, default='{}')
-
-
-# Add a token upon user creation
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(author=instance)
--- orapp/urls.py
+++ orapp/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
 from orapp import views
 from rest_framework.routers import DefaultRouter
-import rest_framework.authtoken.views
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -13,11 +12,5 @@ router.register(r'users', views.UserViewSet)
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
-
-
-# Allows to get a token by sending credentials
-urlpatterns += [
-    url(r'^api-token-auth/', rest_framework.authtoken.views.obtain_auth_token)
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
